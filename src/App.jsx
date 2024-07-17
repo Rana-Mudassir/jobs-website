4e2fdc7df7020959f26f3d63dde4dddfcadcf3c2
Rana-Mudassir <mudasire38@gmail.com>
2024-07-17 14:22:11 +0500

    Add JobPage

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,21 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPAge from "./pages/NotFoundPAge";
+import Jobpage, { jobLoader } from "./pages/Jobpage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<Jobpage />} loader={jobLoader} />
       <Route path="*" element={<NotFoundPAge />} />
     </Route>
   )
 );
 
 const App = () => {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
